143d5308be9e7b4400942243cf97738f291d0042
dhaura <57411348+dhaura@users.noreply.github.com>
2020-04-17 20:04:17 +0530

    Update basicECGRequestDisplay.php

--- Patients/labTests/requestDisplay/basicECGRequestDisplay.php
+++ Patients/labTests/requestDisplay/basicECGRequestDisplay.php
@@ -8,50 +8,77 @@
   <head>
     <meta charset="utf-8">
     <title>Basci ECG Request</title>
+    <link href='http://fonts.googleapis.com/css?family=Bitter' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="../../css/labTestRequests.css">
   </head>
   <body>
-    <h1>CARDIAC INVESTIGATION UNIT</h1>
-    <h2>Basic ECG Request</h2>
-    <?php
-      $nic = '982753295V';  // this should be given by a session object
-      $patientViewObject = new PatientView();
+    <div class="form-style">
+      <h1>CARDIAC INVESTIGATION UNIT</h1>
+      <h2>Basic ECG Request</h2>
+      <?php
+        $nic = '982753295V';  // this should be given by a session object
+        $patientViewObject = new PatientView();
 
-      $results = $patientViewObject->showLabTestsRequests($nic);
+        $results = $patientViewObject->showLabTestsRequests($nic);
 
-      $serializedBasicECGRequest= $results[0]['serializedBasicECGRequest'];
+        $serializedBasicECGRequest= $results[0]['serializedBasicECGRequest'];
 
-      $basicECGRequestObject = unserialize($serializedBasicECGRequest);
+        $basicECGRequestObject = unserialize($serializedBasicECGRequest);
 
-      echo "<b>Date : </b>".$basicECGRequestObject->getProperty('dateOfRequest')."<br><br>";
+        echo '<div class="section"><span>1</span>Date</div>
+        <div class="inner-wrap">
+          '.$basicECGRequestObject->getProperty('dateOfRequest').'
+        </div>
+        <br><br>';
 
-      echo "<b>Service No : </b>".$basicECGRequestObject->getProperty('force_id')."<br><b>NIC : </b>".$basicECGRequestObject->getProperty('nic')."<br><b>Rank : </b>".$basicECGRequestObject->getProperty('dateOfRequest')."
-      <br><b>Name : </b>".$basicECGRequestObject->getProperty('first_name')." ".$basicECGRequestObject->getProperty('last_name')."
-      <br><b>Unit : </b>".$basicECGRequestObject->getProperty('unit')."<br><br>";
-
-      echo "<b>Age : </b>".$basicECGRequestObject->getProperty('age')."<br><br>";
-
-      echo "<b>Gender : </b>".$basicECGRequestObject->getProperty('dateOfRequest')."<br><br>";
-
-      echo "<b>Ward No : </b>".$basicECGRequestObject->getProperty('ward_no')."<br>
-      <b>Ward : </b>".$basicECGRequestObject->getProperty('ward')."<br><br>";
-
-      for ($i=0; $i < count($basicECGRequestObject->getProperty('conditions')) ; $i++) {
-        echo "<b>(*)".($basicECGRequestObject->getProperty('conditions'))[$i]."</b><br>";
-      }
-      echo "<br>";
-      for ($i=0; $i < count($basicECGRequestObject->getProperty('leads')) ; $i++) {
-        if ($basicECGRequestObject->getProperty('leads')[$i] != "") {
-          echo "<b>(*)".($basicECGRequestObject->getProperty('leads'))[$i]."</b><br>";
-        }
-      }
-
-      echo "<br><b>Short History of case : </b>".$basicECGRequestObject->getProperty('shortHistory')."<br><br><br>";
-
-      echo "<b>Name of Consultant/MO : </b>".$basicECGRequestObject->getProperty('consMOName')."<br><b> NIC of Consultant/MO : </b>".$basicECGRequestObject->getProperty('consMOID');
+        echo '<div class="section"><span>2</span>Personal Info</div>
+        <div class="inner-wrap">
+            <label>Service No : </label>'.$basicECGRequestObject->getProperty('force_id').'<br><br><label>NIC : </label>'.$basicECGRequestObject->getProperty('nic').'<br><br><label>Rank : </label>'.$basicECGRequestObject->getProperty('rank').'
+            <br><br><label>Name : </label>'.$basicECGRequestObject->getProperty('first_name').' '.$basicECGRequestObject->getProperty('last_name').'
+            <br><br><label>Unit : </label>'.$basicECGRequestObject->getProperty('unit').'<br>
 
 
-    ?>
+          <br><label>Age : </label>'.$basicECGRequestObject->getProperty('age').'<br>
 
+
+          <br><label>Gender : </label>'.$basicECGRequestObject->getProperty('gender').'<br>
+
+
+        </div><br><br>';
+
+      ?>
+      <div class="section"><span>3</span>Medical Info</div>
+      <div class="inner-wrap">
+          <?php
+            echo "<label>Ward No : </label>".$basicECGRequestObject->getProperty('ward_no')."<br><br>
+            <label>Ward : </label>".$basicECGRequestObject->getProperty('ward')."<br><br>";
+
+            for ($i=0; $i < count($basicECGRequestObject->getProperty('conditions')) ; $i++) {
+              echo "<div class='section'><span>*</span></div><b>".($basicECGRequestObject->getProperty('conditions'))[$i]."</b><br>";
+            }
+            echo "<br>";
+            for ($i=0; $i < count($basicECGRequestObject->getProperty('leads')) ; $i++) {
+              if ($basicECGRequestObject->getProperty('leads')[$i] != "") {
+                echo "<div class='section'><span>*</span></div><b>".($basicECGRequestObject->getProperty('leads'))[$i]."</b><br>";
+              }
+            }
+
+
+            echo "<br><label>Short History of Case : </label>".$basicECGRequestObject->getProperty('shortHistory')."<br><br><br>";
+          ?>
+      </div><br><br>
+
+      <div class="section"><span>4</span>Doctor's Info</div>
+      <div class="inner-wrap">
+        <?php
+
+          echo "<label>Name of Consultant/MO : </label>".$basicECGRequestObject->getProperty('consMOName')."<br><br><label> NIC of Consultant/MO : </label>".$basicECGRequestObject->getProperty('consMOID');
+
+        ?>
+
+      </div>
+
+    </div>
 
   </body>
 </html>

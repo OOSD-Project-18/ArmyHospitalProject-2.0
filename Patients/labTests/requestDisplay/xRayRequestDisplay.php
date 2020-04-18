ca930ba9b8c1ab388a51953f42f6d702ae05a548
dhaura <57411348+dhaura@users.noreply.github.com>
2020-04-18 21:51:32 +0530

    Update xRayRequestDisplay.php

--- Patients/labTests/requestDisplay/xRayRequestDisplay.php
+++ Patients/labTests/requestDisplay/xRayRequestDisplay.php
@@ -8,33 +8,54 @@
   <head>
     <meta charset="utf-8">
     <title>X Ray Request</title>
+    <link href='http://fonts.googleapis.com/css?family=Bitter' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="../../css/labTestRequests.css">
   </head>
   <body>
-    <h1>Army Hospital</h1>
-    <h2>X Ray Request</h2>
-    <?php
-      $nic = '982753295V';  // this should be given by a session object
-      $patientViewObject = new PatientView();
+    <div class="form-style">
+      <h1>Army Hospital</h1>
+      <h2>X Ray Request</h2>
+      <?php
+        $nic = '982753295V';  // this should be given by a session object
+        $patientViewObject = new PatientView();
 
-      $results = $patientViewObject->showLabTestsRequests($nic);
+        $results = $patientViewObject->showLabTestsRequests($nic);
 
-      $serializedXRayRequest = $results[0]['serializedXRayRequest'];
+        $serializedXRayRequest = $results[0]['serializedXRayRequest'];
 
-      $xRayRequestObject = unserialize($serializedXRayRequest);
-
-      echo "<b>Date : </b>".$xRayRequestObject->getProperty('dateOfRequest')."<br><br>";
-
-      echo "<b>Service No : </b>".$xRayRequestObject->getProperty('force_id')."<br><b>NIC : </b>".$xRayRequestObject->getProperty('nic')."<br><b>Rank : </b>".$xRayRequestObject->getProperty('rank')."
-      <br><b>Name : </b>".$xRayRequestObject->getProperty('first_name')." ".$xRayRequestObject->getProperty('last_name')."
-      <br><b>Unit / Ship / A.F.Stn. : </b>".$xRayRequestObject->getProperty('unit')."<br><br>";
-
-      echo "<b>Age : </b>".$xRayRequestObject->getProperty('age')."<br><br>
-      <b>Part to be X-Rayed : </b>".$xRayRequestObject->getProperty('xRayPart')."<br><br>
-      <b>Short History of Case (To be completed by M.O.I. / c case) : </b>".$xRayRequestObject->getProperty('shortHistory')."<br><br>";
-
-      echo "<b>Name of Consultant/MO : </b>".$xRayRequestObject->getProperty('consMOName')."<br><b> Designation of Consultant/MO : </b>".$xRayRequestObject->getProperty('designation')."<br><b> NIC of Consultant/MO : </b>".$xRayRequestObject->getProperty('consMOID');
+        $xRayRequestObject = unserialize($serializedXRayRequest);
 
       ?>
+
+      <div class="section"><span>1</span>Date</div>
+      <div class="inner-wrap">
+        <?php echo $xRayRequestObject->getProperty('dateOfRequest'); ?><br><br>
+      </div>
+
+      <div class="section"><span>2</span>Personal Info</div>
+      <div class="inner-wrap">
+
+        <label>Service No : </label><?php echo $xRayRequestObject->getProperty('force_id'); ?><br><br><label>NIC : </label><?php echo $xRayRequestObject->getProperty('nic'); ?><br><br><label>Rank : </label><?php echo $xRayRequestObject->getProperty('rank'); ?>
+        <br><br><label>Name : </label><?php echo $xRayRequestObject->getProperty('first_name')." ".$xRayRequestObject->getProperty('last_name'); ?>
+        <br><br><label>Unit / Ship / A.F.Stn. : </label><?php echo $xRayRequestObject->getProperty('unit'); ?><br><br>
+
+
+        <label>Age : </label><?php echo $xRayRequestObject->getProperty('age'); ?><br><br>
+      </div>
+
+      <div class="section"><span>3</span>Medical Info</div>
+      <div class="inner-wrap">
+        <label>Part to be X-Rayed : </label><?php echo $xRayRequestObject->getProperty('xRayPart'); ?><br><br>
+        <label>Short History of Case (To be completed by M.O.I. / c case) : </label><?php echo $xRayRequestObject->getProperty('shortHistory'); ?><br><br>
+      </div>
+
+      <div class="section"><span>4</span>Doctor's Info</div>
+      <div class="inner-wrap">
+
+        <label>Name of Consultant/MO : </label><?php echo $xRayRequestObject->getProperty('consMOName'); ?><br><br><label> Designation of Consultant/MO : </label><?php echo $xRayRequestObject->getProperty('designation'); ?><br><br><label> NIC of Consultant/MO : </label><?php echo $xRayRequestObject->getProperty('consMOID'); ?>
+
+      </div>
+    </div>
 
   </body>
 </html>

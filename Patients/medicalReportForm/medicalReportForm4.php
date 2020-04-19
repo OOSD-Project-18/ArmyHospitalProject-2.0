edc5b52139dd3694e7510d2c0411adc95cb2e032
dhaura <57411348+dhaura@users.noreply.github.com>
2020-04-19 19:19:10 +0530

    Update medicalReportForm4.php

--- Patients/medicalReportForm/medicalReportForm4.php
+++ Patients/medicalReportForm/medicalReportForm4.php
@@ -4,27 +4,25 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Medical Report</title>
-    <link rel="stylesheet" href="../css/medicalReportForm.css">
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-
+    <link href='http://fonts.googleapis.com/css?family=Bitter' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="../../css/medicalReport2.css">
 </head>
 <body>
-  <header>
-    <h2>Medical Report Form - Page 4</h2>
-  </header>
+  <div class="medical-report">
+    <h1>Medical Report</h1><br><br>
+    <div class="inner-wrap">
+      <form action="includes/form4.inc.php" method="POST">
+        <label>Medical Examiner's Summary of important points in 3 and 4 above with comments and additional information of significance :</label>
+        <textarea rows = "20" cols = "60" name = "summary">
+        </textarea><br><br>
 
-  <div class="form">
+        <div class="button-section">
+          <button type="submit" name="next">Next</button>
+        </div>
 
-<form action="includes/form4.inc.php" method="POST">
-<p>Medical Examiner's Summary of important points in 2 and 3 above with comments and additional information of significance </p>
-<textarea rows = "10" cols = "60" name = "summary">
-</textarea><br><br>
-<button type="submit" name ="next">Next</button>
+      </form>
+    </div>
+  </div>
 
-</form>
-</div>
 </body>
 </html>

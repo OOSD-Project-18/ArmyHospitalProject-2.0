c8e1797f34438e8f0dc0a041381dca22fa7c00db
dhaura <57411348+dhaura@users.noreply.github.com>
2020-04-21 20:51:01 +0530

    Update medicalReportDisplay5.php

--- Patients/medicalReportForm/medicalReportDisplay5.php
+++ Patients/medicalReportForm/medicalReportDisplay5.php
@@ -7,9 +7,10 @@
   <head>
     <meta charset="utf-8">
     <title>Medical Report</title>
+    <link href='http://fonts.googleapis.com/css?family=Bitter' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="../css/medicalReport.css">
   </head>
   <body>
-    <h2>Eyes</h2>
     <?php
       $results = $_SESSION['results'];
 
@@ -21,24 +22,32 @@
       $diseases = $eyes->getDiseases();
       $effect = $eyes->getEffect();
 
-      echo '<table border="1">
-        <tr>
-          <th></th> <th>Right</th><th>Left</th><th>CP</th>
-        </tr>
-        <tr><th>Without Glasses</th><td>'.$eyeTable['right'].'</td><td>'.$eyeTable['left'].'</td><td>'.$eyeTable['CP'].'</td>
-
-        <tr><td>'.$eyeTable['emptyHeading'].'</td><td>'.$eyeTable['emptyRight'].'</td><td>'.$eyeTable['emptyLeft'].'</td><td>'.$eyeTable['emptyCP'].'</td></tr>
-
-      </table>
-      <p><b>Diseases, etc : </b></p>
-      '.$diseases.'<br><br><br>
-
-      <b>Effect on P. if any : </b> '.$effect.'<br><br><br>';
-
      ?>
 
-     <form action="medicalReportDisplay6.php" method="post">
-       <button type="submit" name="next">Next</button>
-     </form>
+     <div class="medical-report">
+       <h1>Medical Report</h1>
+       <form action="includes/form5.inc.php" method="post">
+         <div class="section"><span>6</span>Eyes</div>
+         <div class="inner-wrap">
+           <table class='table'>
+             <tr>
+               <th></th> <th>Right</th><th>Left</th><th>CP</th>
+             </tr>
+             <tr><th>Without Glasses</th><td><?php echo $eyeTable['right']; ?></td><td><?php echo $eyeTable['left']; ?></td><td><?php echo $eyeTable['CP']; ?></td>
+
+             <tr><td><?php echo $eyeTable['emptyHeading']; ?></td><td><?php echo $eyeTable['emptyRight']; ?></td><td><?php echo $eyeTable['emptyLeft']; ?></td><td><?php echo $eyeTable['emptyCP']; ?></td></tr>
+           </table><br><br>
+
+           <label>Diseases, etc : </label><?php echo $diseases; ?><br><br>
+
+           <label>Effect on P. if any : </label><?php echo $effect; ?><br><br>
+
+           <br>
+           <div class="button-section">
+             <button type="submit" name="next">Next</button>
+           </div>
+         </div>
+       </form>
+     </div>
   </body>
 </html>

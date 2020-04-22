ac85e10daf872f3e0872745b9b4ddb69693a98f1
dhaura <57411348+dhaura@users.noreply.github.com>
2020-04-22 20:32:21 +0530

    Update medicalReportDisplay6.php

--- Patients/medicalReportForm/medicalReportDisplay6.php
+++ Patients/medicalReportForm/medicalReportDisplay6.php
@@ -8,9 +8,10 @@
   <head>
     <meta charset="utf-8">
     <title>Medical Report</title>
+    <link href='http://fonts.googleapis.com/css?family=Bitter' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="../css/medicalReport.css">
   </head>
   <body>
-    <h2>Ears, Nose, Throat</h2>
     <?php
       $results = $_SESSION['results'];
 
@@ -23,37 +24,50 @@
       $diseases = $earsNoseThroatObject->getDiseases();
       $h = $earsNoseThroatObject->getH();
       $effect = $earsNoseThroatObject->getEffect();
-
-      echo '<h3>Hearing</h3>
-      <table border="1">
-        <tr> <th></th> <th>Hears F.W. at </th><th>Hears C.V. at </th> </tr>
-        <tr><th>Right</th><td>'.$hearing['rfw'].'</td><td>'.$hearing['rcv'].'</td></tr>
-        <tr><th>Left</th><td>'.$hearing['lfw'].'</td><td>'.$hearing['lcv'].'</td></tr>
-        <tr><th>Both</th><td>'.$hearing['bothfw'].'</td><td>'.$hearing['bothcv'].'</td></tr>
-      </table>
-
-      <table border="1">
-        <h4> Wax </h4>
-        <tr> <th></th> <th>Present </th><th>Removed</th> </tr>
-        <tr><th>Right</th><td>'.$wax['rpresent'].'</td><td>'.$wax['rremoved'].'</td></tr>
-        <tr><th>Left</th><td>'.$wax['lpresent'].'</td><td>'.$wax['lremoved'].'</td></tr>
-      </table>
-
-      <h3>Diseases, etc : </h3>
-      '.$diseases.'
-      <br><br><br>
-      <table border="1">
-      <tr><th> H </th> </tr>
-      <tr><td>'.$h.'</td></tr>
-      </table>
-      <br><b>
-      Effect on P. if any : </b>'.$effect.'<br><br><br>';
-
-
      ?>
 
-     <form action="medicalReportDisplay7.php" method="post">
-       <button type="submit" name="next">Next</button>
-     </form>
+     <div class="medical-report">
+      <h1>Medical Report</h1><br>
+        <div class="section"><span>7</span>Ears, Nose, Throat</div>
+        <div class="inner-wrap">
+          <label>Hearing : </label>
+          <table class='table'>
+            <tr> <th></th> <th>Hears F.W. at </th><th>Hears C.V. at </th> </tr>
+            <tr><th>Right</th><td><?php echo $hearing['rfw']; ?></td><td><?php echo $hearing['rcv']; ?></td></tr>
+            <tr><th>Left</th><td> <?php echo $hearing['lfw']; ?></td><td><?php echo $hearing['lcv']; ?></td></tr>
+            <tr><th>Both</th><td> <?php echo $hearing['bothfw']; ?></td><td><?php echo $hearing['bothcv']; ?></td></tr>
+
+          </table>
+          <br>
+          <label>Wax : </label>
+          <table class='table'>
+
+            <tr> <th></th> <th>Present </th><th>Removed</th> </tr>
+
+            <tr><th>Right</th><td><?php echo $wax['rpresent']; ?></td><td><?php echo $wax['rremoved']; ?></td></tr>
+            <tr><th>Left</th><td> <?php echo $wax['lpresent']; ?></td><td><?php echo $wax['lremoved']; ?></td></tr>
+
+          </table>
+
+          <br>
+          <label>Diseases, etc : </label>
+          <?php echo $diseases; ?><br><br>
+
+          <table class='table'>
+          <tr><th> H </th> </tr>
+
+          <tr><td><?php echo $h; ?></td></tr>
+
+          </table>
+          <br>
+         <label>Effect on P. if any : </label><?php echo $effect; ?><br>
+          <br><br>
+          <div class="button-section">
+            <form action="medicalReportDisplay7.php" method="post">
+              <button type="submit" name="next">Next</button>
+            </form>
+          </div>
+        </div>
+     </div>
   </body>
 </html>

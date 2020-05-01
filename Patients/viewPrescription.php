cd162657522d29995015652b3a56c897481ffffb
Thushani-Jayasekera <thushanij.18@cse.mrt.ac.lk>
2020-05-01 12:15:17 +0530

    include and update css

--- Patients/viewPrescription.php
+++ Patients/viewPrescription.php
@@ -1,6 +1,12 @@
 <?php
-  include_once "includes/class-autoload.inc.php";
-  session_start();
+include_once "includes/class-autoload.inc.php";
+
+require_once('includes/initFromPatients.php');
+$user = new User();
+if (!$user->isLoggedIn()) {
+    Redirect::to('../index.php');
+}
+
   $nic = $_SESSION['nic'];
   // $doa = $_SESSION['doa'];
   $patientObj = new PatientView();
@@ -15,7 +21,7 @@
   <head>
     <meta charset="utf-8">
     <title>Prescriptions</title>
-    <link rel="stylesheet" href="css/oldVisits.css">
+    <!--link rel="stylesheet" href="css/oldVisits.css"-->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
@@ -23,65 +29,80 @@
 
   </head>
   <body>
-    <header>
-      <h2>Prescriptions</h2>
-    </header>
-    <div class="form">
+    <?php include_once('_header.php') ?>
+    <main  id="main">
+      <?php include_once('_sideNav.php') ?>
+        <div class="container py-4">
+          <form class="card p-3">
+            <div class="text-center">
+                <h2>Prescriptions</h2>
+            </div>
+          <hr>
+            <div class="form ">
+                <div class="text-center">
+                <h4>Current Prescription</h4>
+                </div>
+                  <?php
+                      if(empty($currentPrescription)){
+                        echo "<h6> No unissued prescriptions <h6>";
 
-    <h2>Current Prescription</h2>
-    <?php
-        if(empty($currentPrescription)){
-          echo "<h5> No unissued prescriptions <h5>";
+                      } else{
+                        echo "<table class='table table-striped' id='prescription-table'>
+                          <form action='includes/viewPrescription.inc.php' method='post'>
+                          <thead>
+                            <tr>
+                              <th scope='col'>Date</th><th scope='col'>Prescripton</th><th scope='col'>Click when issued</th>
+                            </tr>
+                          </thead>
+                          <tbody>";
+                        $x = 0;
+                      foreach($currentPrescription as $result){
+                        $prescriptionObj = unserialize($result['Prescription']);
+                        $_SESSION['prescription-'.$x] = $result['Prescription'];
+                        $prescription = $prescriptionObj->getPrescription();
+                        $date = $result['doa'];
+                        echo "<tr><td>$date</td><td>$prescription</td><td><button type='submit' name='button-".$x."'>Issue</button></td></tr>";
+                        $x++;
+                      }
+                      $_SESSION['x'] = $x;
+                    }
 
-        } else{
-          echo "<table class='content-table' id='prescription-table' border='1'>
-            <form action='includes/viewPrescription.inc.php' method='post'>
-            <thead>
-              <tr>
-                <th>Date</th><th>Prescripton</th><th>Click when issued</th>
-              </tr>
-            </thead>
-            <tbody>";
-          $x = 0;
-        foreach($currentPrescription as $result){
-          $prescriptionObj = unserialize($result['Prescription']);
-          $_SESSION['prescription-'.$x] = $result['Prescription'];
-          $prescription = $prescriptionObj->getPrescription();
-          $date = $result['doa'];
-          echo "<tr><td>$date</td><td>$prescription</td><td><button type='submit' name='button-".$x."'>Issue</button></td></tr>";
-          $x++;
-        }
-        $_SESSION['x'] = $x;
-      }
+                    ?>
+                  </tbody>
+                  </table><br>
+              
+            </div>
+    
+    <hr>
+            <div class="form">
+              <div class="text-center">
+              <h4>Issued Prescriptions</h4>
+              </div>
 
-        ?>
-      </tbody>
-    </table><br>
-    </form>
+                <table class='table table-striped' id='prescription-table'>
+                  <thead>
+                    <tr>
+                      <th scope="col" >Date</th><th scope="col table-primary">Prescripton</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <?php
+                    foreach($allPrescriptions as $result){
+                      $prescriptionObj = unserialize($result['Prescription']);
+                      $prescription = $prescriptionObj->getPrescription();
+                      $date = $result['doa'];
+                      echo "<tr><td>$date</td><td>$prescription</td></tr>";
+                    }
 
-    <h2>Issued Prescriptions</h2>
-
-    <table class='content-table' id='prescription-table' border='1'>
-      <thead>
-        <tr>
-          <th>Date</th><th>Prescripton</th>
-        </tr>
-      </thead>
-      <tbody>
-        <?php
-        foreach($allPrescriptions as $result){
-          $prescriptionObj = unserialize($result['Prescription']);
-          $prescription = $prescriptionObj->getPrescription();
-          $date = $result['doa'];
-          echo "<tr><td>$date</td><td>$prescription</td></tr>";
-        }
-
-        ?>
-      </tbody>
-    </table>
+                    ?>
+                  </tbody>
+                </table>
 
 
+            </div>
+   
+   </form>
    </div>
-
+        </main>
   </body>
 </html>

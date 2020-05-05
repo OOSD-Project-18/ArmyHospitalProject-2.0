5985ba80c8c21fbec69a321ebebd6d5e803173e0
Thushani-Jayasekera <thushanij.18@cse.mrt.ac.lk>
2020-05-05 13:22:36 +0530

    edit

--- Patients/medicalReportForm/medicalReportDisplay3.php
+++ Patients/medicalReportForm/medicalReportDisplay3.php
@@ -1,6 +1,13 @@
 <?php
-  session_start();
+  //session_start();
   include_once "classes/otherMedicalTreatments.class.php";
+  include_once "../includes/class-autoload.inc.php";
+  require_once '../includes/initFromPatients.php';
+  $user=new User();
+  if(!$user->isLoggedIn()){
+      Redirect::to('../index.php');
+  }
+
 ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
@@ -9,8 +16,16 @@
     <title>Medical Report</title>
     <link href='http://fonts.googleapis.com/css?family=Bitter' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="../css/medicalReport2.css">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
   </head>
   <body>
+  <?php include('_header.med.php') ?>
+    <main  id=main>
+        <?php include('_sideNav.med.php') ?>
     <div class="medical-report">
       <h1>Medical Report</h1><br><br>
       <div class="section"><span>5</span>Other Medical Treatments at Home or in a Nursing Home</div>
@@ -59,5 +74,6 @@
         </div>
       </div>
     </div>
+    </main>
   </body>
 </html>

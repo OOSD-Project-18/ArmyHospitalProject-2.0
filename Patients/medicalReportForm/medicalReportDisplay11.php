0b0c69d6835ed844e7f471ae027560d6e4b4d41a
Thushani-Jayasekera <thushanij.18@cse.mrt.ac.lk>
2020-05-05 14:45:29 +0530

    edit

--- Patients/medicalReportForm/medicalReportDisplay11.php
+++ Patients/medicalReportForm/medicalReportDisplay11.php
@@ -1,6 +1,12 @@
 <?php
-  session_start();
+  //session_start();
   include_once "classes/specialistReport.class.php";
+  include_once "../includes/class-autoload.inc.php";
+  require_once '../includes/initFromPatients.php';
+  $user=new User();
+  if(!$user->isLoggedIn()){
+      Redirect::to('../index.php');
+  }
 ?>
 
 <!DOCTYPE html>
@@ -10,8 +16,16 @@
     <title>Medical Report</title>
     <link href='http://fonts.googleapis.com/css?family=Bitter' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="../css/medicalReport2.css">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
   </head>
   <body>
+  <?php include('_header.med.php') ?>
+    <main  id=main>
+        <?php include('_sideNav.med.php') ?>
     <?php
       $results = $_SESSION['results'];
 
@@ -47,6 +61,7 @@
              <br><label>Date : </label><?php echo $dateOfReport; ?><br><br>
 
              <div class="section"><span>a</span><p><b>Initial PULHEEMS Assessment</b></p></div><br>
+             <div style="overflow-x:scroll;">
              <table class='table'>
                <tr>
                  <th>Y.O.B</th><th>P</th><th>U</th><th>L</th><th>H</th><th>E</th><th>E</th><th>M</th><th>S</th>
@@ -55,6 +70,7 @@
                  <td><?php echo $initPulheems[0]; ?></td><td><?php echo $initPulheems[1][0]; ?></td><td><?php echo $initPulheems[1][1]; ?></td><td><?php echo $initPulheems[1][2]; ?></td><td><?php echo $initPulheems[1][3]; ?></td><td><?php echo $initPulheems[1][4]; ?></td><td><?php echo $initPulheems[1][5]; ?></td><td><?php echo $initPulheems[1][6]; ?></td><td><?php echo $initPulheems[1][7]; ?></td>
                </tr>
              </table><br><br>
+</div>
              <label>Height(inches) : </label><?php echo $initPulheems[2]; ?><br><br>
              <label>CP.            : </label><?php echo $initPulheems[3]; ?><br><br>
              <label>Weight(lbs)    : </label><?php echo $initPulheems[4]; ?><br><br>
@@ -67,6 +83,7 @@
              <label>Date : </label><?php echo $initPulheems[9]; ?><br><br>
 
              <div class="section"><span>b</span><p><b>Service PULHEEMS Assessment</b></p></div><br>
+             <div style="overflow-x:scroll;">
              <table class='table'>
                <tr>
                  <th>Y.O.B</th><th>P</th><th>U</th><th>L</th><th>H</th><th>E</th><th>E</th><th>M</th><th>S</th>
@@ -75,6 +92,7 @@
                  <td><?php echo $servicePulheems[0]; ?></td><td><?php echo $servicePulheems[1][0]; ?></td><td><?php echo $servicePulheems[1][1]; ?></td><td><?php echo $servicePulheems[1][2]; ?></td><td><?php echo $servicePulheems[1][3]; ?></td><td><?php echo $servicePulheems[1][4]; ?></td><td><?php echo $servicePulheems[1][5]; ?></td><td><?php echo $servicePulheems[1][6]; ?></td><td><?php echo $servicePulheems[1][7]; ?></td>
                </tr>
              </table><br>
+</div>
              <label>Height(inches) : </label><?php echo $servicePulheems[2]; ?><br><br>
              <label>CP. : </label> <?php echo $servicePulheems[3]; ?><br><br>
              <label>Weight(lbs) : </label><?php echo $servicePulheems[4]; ?><br><br>
@@ -92,5 +110,6 @@
          </form>
        </div>
      </div>
+     </main>
   </body>
 </html>

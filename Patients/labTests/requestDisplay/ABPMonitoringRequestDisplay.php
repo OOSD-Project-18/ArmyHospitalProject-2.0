1093c746e5da1276d67b08618bcd656adb7fdccc
Thushani-Jayasekera <thushanij.18@cse.mrt.ac.lk>
2020-05-11 00:11:31 +0530

    css !responsive

--- Patients/labTests/requestDisplay/ABPMonitoringRequestDisplay.php
+++ Patients/labTests/requestDisplay/ABPMonitoringRequestDisplay.php
@@ -1,7 +1,15 @@
 <?php
-  session_start();
+ 
   include_once "../../classes/patientview.class.php";
   include_once "../classes/ABPMonitoringRequest.class.php";
+  include_once "../../classes/patientview.class.php";
+  include_once "../../includes/class-autoload.inc.php";
+  require_once '../../includes/initFromPatients.php';
+  //session_start(); This is done by above file
+  $user=new User();
+  if(!$user->isLoggedIn()){
+      Redirect::to('../../index.php');
+  }
 ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
@@ -10,8 +18,18 @@
     <title>ABP Monitoring Request</title>
     <link href='http://fonts.googleapis.com/css?family=Bitter' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="../../css/labTestRequests.css">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
+
   </head>
   <body>
+  <?php include('../_header.lab.php') ?>
+    <main  id=main>
+        <?php include('../_sideNav.lab.php') ?>
+    <div class="container py-4">
     <div class="form-style">
       <h1>CARDIAC INVESTIGATION UNIT</h1>
       <h2>ABP Monitoring Request</h2>
@@ -52,7 +70,7 @@
         <br><br><label>Weight : </label><?php echo $ABPMonitoringRequestObject->getProperty('weight'); ?>
 
         <?php
-          echo '<br><br><label>Contact No : </label><input type="number" name="contact" value='.$ABPMonitoringRequestObject->getProperty('mobile').'>';
+          echo '<br><br><label>Contact No : </label><input type="number" name="contact" value='.$ABPMonitoringRequestObject->getProperty('contact').'>';
          ?>
       </div>
 
@@ -76,6 +94,8 @@
       <br><br><label>Time : </label><?php echo $ABPMonitoringRequestObject->getProperty('time'); ?>
 
     </div>
+    </div>
+    </main>
 
   </body>
 </html>
--- Patients/labTests/requestDisplay/immunoassayRequestDisplay.php
+++ Patients/labTests/requestDisplay/immunoassayRequestDisplay.php
@@ -1,7 +1,13 @@
 <?php
-  session_start();
+  //session_start();
   include_once "../../classes/patientview.class.php";
   include_once "../classes/immunoassayRequest.class.php";
+  include_once "../../includes/class-autoload.inc.php";
+  require_once '../../includes/initFromPatients.php';
+  $user=new User();
+  if(!$user->isLoggedIn()){
+      Redirect::to('../../index.php');
+  }
 ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
@@ -10,8 +16,18 @@
     <title>Immunoassay Request</title>
     <link href='http://fonts.googleapis.com/css?family=Bitter' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="../../css/labTestRequests.css">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
   </head>
   <body>
+  <?php include('../_header.lab.php') ?>
+    <main  id=main>
+        <?php include('../_sideNav.lab.php') ?>
+    <div class="container py-4">
+    
     <div class="form-style">
       <h1>Army Hospital</h1>
       <h2>Immunoassay Request</h2>
@@ -113,6 +129,8 @@
 
       </div>
     </div>
+    </div>
+    </main>
 
   </body>
 </html>

647e1dc273c1ed56ed0f6a4178d229239d5c559e
Thushani-Jayasekera <thushanij.18@cse.mrt.ac.lk>
2020-05-10 23:44:53 +0530

    CSS !responsive

--- Patients/labTests/labTestsRequests/holterMonitoringRequest.php
+++ Patients/labTests/labTestsRequests/holterMonitoringRequest.php
@@ -1,7 +1,13 @@
 <?php
-  session_start();
-  include_once "../../includes/class-autoload.inc.php";
+  //session_start();
   include_once "../../classes/patientview.class.php";
+  include_once "../../includes/class-autoload.inc.php";
+  require_once '../../includes/initFromPatients.php';
+  //session_start(); This is done by above file
+  $user=new User();
+  if(!$user->isLoggedIn()){
+      Redirect::to('../../index.php');
+  }
 
 ?>
 <!DOCTYPE html>
@@ -11,9 +17,19 @@
     <title>Holter Monitoring Request</title>
     <link href='http://fonts.googleapis.com/css?family=Bitter' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="../../css/labTestRequests.css">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
 
   </head>
   <body>
+  <?php include('../_header.lab.php') ?>
+    <main  id=main>
+        <?php include('../_sideNav.lab.php') ?>
+    <div class="container py-4">
+    <div class="form-style">
     <h1>CARDIAC INVESTIGATION UNIT</h1>
     <h2>Holter Monitoring Request</h2>
     <?php
@@ -28,7 +44,7 @@
     <form action="../includes/HolterMonitoringRequest.inc.php" method="post">
       <b>Date : </b><input type="date" name="dateOfRequest"><br><br>
       <?php
-        echo "<b>Service No : </b>".$patientInfo['force_id']."<br><b>NIC : </b>".$patientInfo['nic']."<br><b>Rank : </b>".$patientInfo['rank']."
+        echo "<b>Service No : </b>".$patientInfo['force_id']."<br><b>NIC : </b>".$patientInfo['NIC']."<br><b>Rank : </b>".$patientInfo['rank']."
         <br><b>Name : </b>".$patientInfo['first_name']." ".$patientInfo['last_name']."
         <br><b>Unit : </b>".$patientInfo['regiment']."<br><br>";
       ?>
--- Patients/labTests/labTestsRequests/immunoassayRequest.php
+++ Patients/labTests/labTestsRequests/immunoassayRequest.php
@@ -1,6 +1,14 @@
 <?php
-  session_start();
+  //session_start();
   include_once "../../classes/patientview.class.php";
+  include_once "../../includes/class-autoload.inc.php";
+  require_once '../../includes/initFromPatients.php';
+  //session_start(); This is done by above file
+  $user=new User();
+  if(!$user->isLoggedIn()){
+      Redirect::to('../../index.php');
+  }
+
 ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
@@ -9,8 +17,17 @@
     <title>Immunoassay Request</title>
     <link href='http://fonts.googleapis.com/css?family=Bitter' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="../../css/labTestRequests.css">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
   </head>
   <body>
+  <?php include('../_header.lab.php') ?>
+    <main  id=main>
+        <?php include('../_sideNav.lab.php') ?>
+    <div class="container py-4">
     <div class="form-style">
       <h1>Army Hospital</h1>
       <h2>Immunoassay Request</h2>
@@ -33,7 +50,7 @@
         <div class="section"><span>2</span>Personal Info</div>
         <div class="inner-wrap">
           <?php
-            echo "<label>Service No : </label>".$patientInfo['force_id']."<br><br><label>NIC : </label>".$patientInfo['nic']."<br><br><label>Rank : </label>".$patientInfo['rank']."
+            echo "<label>Service No : </label>".$patientInfo['force_id']."<br><br><label>NIC : </label>".$patientInfo['NIC']."<br><br><label>Rank : </label>".$patientInfo['rank']."
             <br><br><label>Name : </label>".$patientInfo['first_name']." ".$patientInfo['last_name']."
             <br><br><label>Unit : </label>".$patientInfo['regiment']."<br><br>
 

e4e00f2e05d9b71f73a0eab52191b0f2280dfd7c
dhaura <57411348+dhaura@users.noreply.github.com>
2020-05-12 18:56:36 +0530

    display no records

---
+++ Patients/labTests/noRecords.php
@@ -0,0 +1,21 @@
+<?php
+  session_start();
+?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Basci ECG Request</title>
+    <link href='http://fonts.googleapis.com/css?family=Bitter' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="../css/labTestRequests.css">
+  </head>
+  <body>
+    <div class="form-style">
+      <h1>CARDIAC INVESTIGATION UNIT</h1>
+      <h2>Basic ECG Request</h2>
+      <h2>No Records to Display</h2>
+
+    </div>
+
+  </body>
+</html>

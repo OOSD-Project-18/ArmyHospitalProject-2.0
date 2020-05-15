31d44fbe1f52fc886f32502eb9225c710f659ed1
dhaura <57411348+dhaura@users.noreply.github.com>
2020-05-15 18:19:38 +0530

    Update histopathologyRequest.php

--- labTests/labTestsRequests/histopathologyRequest.php
+++ labTests/labTestsRequests/histopathologyRequest.php
@@ -39,7 +39,7 @@
       <form action="../includes/histopathologyRequest.inc.php" method="post">
         <div class="section"><span>1</span>Date</div>
         <div class="inner-wrap">
-          <input type="date" name="date">
+          <input type="date" name="date" required>
         </div>
 
         <div class="section"><span>2</span>Personal Info</div>
@@ -50,10 +50,10 @@
             <br><br><label>Unit : </label>".$patientInfo['regiment']."<br><br>";
           ?>
 
-          <label>Age : </label><input type="number" name="age" placeholder="Ex: 30 Yrs"><br><br>
+          <label>Age : </label><input type="number" name="age" placeholder="Ex: 30 Yrs" required><br><br>
 
           <?php
-            echo "<label>Family Name : </label><input type='text' name='familyName' value=".$patientInfo['last_name']."><br><br>";
+            echo "<label>Family Name : </label><input type='text' name='familyName' value=".$patientInfo['last_name']." required><br><br>";
             if ($patientInfo['gender'] == 'male') {
               $gender  = 'Male';
             }else {
@@ -63,25 +63,25 @@
 
             $mobile = $patientInfo['mobile'];
 
-            echo '<label>Tel. No : </label><input type="number" name="telNo" value='.$mobile.'><br><br>';
+            echo '<label>Tel. No : </label><input type="number" name="telNo" value='.$mobile.' required><br><br>';
           ?>
         </div>
 
         <div class="section"><span>3</span>Medical Info</div>
         <div class="inner-wrap">
-          <label>Ward : </label><input type="text" name="ward"><br><br>
+          <label>Ward : </label><input type="text" name="ward" required><br><br>
 
-          <label>Specimen type & site : </label><br><textarea name="specimen" rows="4" cols="50"></textarea><br><br>
-          <label>Relevent clinical history (duration etc.) & finding. : </label><br><textarea name="clinicalHistory" placeholder="Eg; Lymphadenopathy, Hepatosplenomegaly" rows="4" cols="50"></textarea><br><br>
-          <label>Relevent investigations : </label><br><textarea name="investigations" rows="4" cols="50"></textarea><br><br>
-          <label>Relevent radiological findings (Including USS/CT/MRI/Mammography) : </label><br><textarea name="radio" rows="4" cols="50"></textarea><br><br>
-          <label>Probe diagnosis / Differential diagnosis : </label><br><textarea name="proDiff" rows="4" cols="50"></textarea><br><br>
-          <label>If a previous biopsy was done (including FNAC/PAP/Ytru cut etc.) give details diagnosis : </label><br><textarea name="prev" rows="4" cols="50"></textarea><br><br>
-          <label>If done at Army Hospital, reference number of pathology report : </label><br><textarea name="reportNo" rows="4" cols="50"></textarea><br><br>
+          <label>Specimen type & site : </label><br><textarea name="specimen" rows="4" cols="50" required></textarea><br><br>
+          <label>Relevent clinical history (duration etc.) & finding. : </label><br><textarea name="clinicalHistory" placeholder="Eg; Lymphadenopathy, Hepatosplenomegaly" rows="4" cols="50" required></textarea><br><br>
+          <label>Relevent investigations : </label><br><textarea name="investigations" rows="4" cols="50" required></textarea><br><br>
+          <label>Relevent radiological findings (Including USS/CT/MRI/Mammography) : </label><br><textarea name="radio" rows="4" cols="50" required></textarea><br><br>
+          <label>Probe diagnosis / Differential diagnosis : </label><br><textarea name="proDiff" rows="4" cols="50" required></textarea><br><br>
+          <label>If a previous biopsy was done (including FNAC/PAP/Ytru cut etc.) give details diagnosis : </label><br><textarea name="prev" rows="4" cols="50" required></textarea><br><br>
+          <label>If done at Army Hospital, reference number of pathology report : </label><br><textarea name="reportNo" rows="4" cols="50" required></textarea><br><br>
           <label>Pre.Op.chemotherapy given : </label> Yes <input type="radio" name="chemo" value="Yes"> No <input type="radio" name="chemo" value="No"><br><br>
           <label>Radiotherapy given : </label> Yes <input type="radio" name="radiotherapy" value="Yes"> No <input type="radio" name="radiotherapy" value="No"><br><br>
-          <label>Relevent opertative findings / endoscopic findings and orientation of the specimen : </label><br><textarea name="findings" rows="4" cols="50"></textarea><br><br>
-          <label>Details of the Consultant / MO to be contacted for further details regarding the surgery/ procedure : </label><br><textarea name="contactDoc" rows="4" cols="50"></textarea><br><br>
+          <label>Relevent opertative findings / endoscopic findings and orientation of the specimen : </label><br><textarea name="findings" rows="4" cols="50" required></textarea><br><br>
+          <label>Details of the Consultant / MO to be contacted for further details regarding the surgery/ procedure : </label><br><textarea name="contactDoc" rows="4" cols="50" required></textarea><br><br>
         </div>
 
         <div class="section"><span>4</span>Doctor's Info</div>
@@ -97,9 +97,9 @@
         </div>
 
 
-        <?php   echo '<br><br><label>Contact No : </label><input type="number" name="contact" value='.$mobile.'><br><br>' ?>
+        <?php   echo '<br><br><label>Contact No : </label><input type="number" name="contact" value='.$mobile.' required><br><br>' ?>
 
-        <label>Extension No Histopathology : </label><input type='number' name='hisNo'> <label> Extension No Reporting Room : </label><input type='number' name='repRoom'>
+        <label>Extension No Histopathology : </label><input type='number' name='hisNo' required> <label> Extension No Reporting Room : </label><input type='number' name='repRoom' required>
 
         <?php
           $_SESSION['nic'] = $nic;

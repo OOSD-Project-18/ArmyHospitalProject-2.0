38f1f3ab2065b830265e83636f005aba2998fa23
dhaura <57411348+dhaura@users.noreply.github.com>
2020-05-15 17:59:48 +0530

    Update holterMonitoringRequest.php

--- labTests/labTestsRequests/holterMonitoringRequest.php
+++ labTests/labTestsRequests/holterMonitoringRequest.php
@@ -38,51 +38,71 @@
     ?>
 
     <form action="../includes/HolterMonitoringRequest.inc.php" method="post">
-      <b>Date : </b><input type="date" name="dateOfRequest"><br><br>
+      <div class="section"><span>1</span>Date</div>
+      <div class="inner-wrap">
+        <input type="date" name="dateOfRequest">
+      </div><br><br>
+
+      <div class="section"><span>2</span>Personal Info</div>
+      <div class="inner-wrap">
+        <?php
+          echo "<label>Regt No : </label>".$patientInfo['force_id']."<br><br><label>NIC : </label>".$patientInfo['NIC']."<br><br><label>Rank : </label>".$patientInfo['rank']."
+          <br><br><label>Name : </label>".$patientInfo['first_name']." ".$patientInfo['last_name']."
+          <br><br><label>Unit : </label>".$patientInfo['regiment']."<br><br>";
+        ?>
+
+        <label>Age : </label><input type="number" name="age" placeholder="Ex: 30 Yrs"><br><br>
+
+        <?php
+
+          if ($patientInfo['gender'] == 'male') {
+            $gender  = 'Male';
+          }else {
+            $gender = 'Female';
+          }
+          echo "<label>Gender : </label>".$gender."<br><br>";
+
+        ?>
+
+        <?php
+          $dob = $patientInfo['date_of_birth'];
+          echo "<b>Date of Birth : </b>".$dob;
+        ?>
+
+        <br><br><label>Height : </label><input type="text" name="height">
+        <br><br><label>Weight : </label><input type="text" name="weight">
+
+        <?php
+          $mobile = $patientInfo['mobile'];
+
+          echo '<br><br><label>Contact No : </label><input type="number" name="contact" value='.$mobile.'>';
+         ?>
+      </div>
+
+      <div class="section"><span>3</span>Medical Info</div>
+      <div class="inner-wrap">
+        <label>Ward No : </label><input type="number" name="ward_no"><br><br>
+        <label>Blood Pressure : </label><input type="number" name="bp"><br><br>
+        <label>Short History of case : </label><textarea name="shortHistory" rows="4" cols="50"></textarea><br><br><br>
+      </div>
+
+      <div class="section"><span>4</span>Doctor's Info</div>
+      <div class="inner-wrap">
+        <?php
+          $consMOName = "Dr. KPN Pathirana"; // should get from a session
+          $consMOID = "687543545v";  // should get from a session
+
+          echo "<label>Name of Consultant/MO : </label>".$consMOName."<br><br><label> NIC of Consultant/MO : </label>".$consMOID;
+
+        ?>
+      </div>
+
+       <br><br><label>Appointed Date : </label><input type="date" name="appointedDate">
+       <br><br><label>Time : </label><input type="time" name="time"><br><br>
+
+
+
       <?php
-        echo "<b>Service No : </b>".$patientInfo['force_id']."<br><b>NIC : </b>".$patientInfo['NIC']."<br><b>Rank : </b>".$patientInfo['rank']."
-        <br><b>Name : </b>".$patientInfo['first_name']." ".$patientInfo['last_name']."
-        <br><b>Unit : </b>".$patientInfo['regiment']."<br><br>";
-      ?>
-
-      <b>Age : </b><input type="number" name="age" placeholder="Ex: 30"><br><br>
-
-      <?php
-        if ($patientInfo['gender'] == 'male') {
-          $gender  = 'Male';
-        }else {
-          $gender = 'Female';
-        }
-        echo "<b>Gender : </b>".$gender."<br><br>";
-      ?>
-
-      <b>Ward No : </b><input type="number" name="ward_no"><br><br>
-
-      <b>Blood Pressure : </b><input type="number" name="bp"><br><br>
-
-      <?php
-        $dob = $patientInfo['date_of_birth'];
-        echo "<b>Date of Birth : </b>".$dob;
-      ?>
-
-      <br><br><b>Height : </b><input type="text" name="height">
-      <br><br><b>Weight : </b><input type="text" name="weight">
-
-      <?php
-        $mobile = $patientInfo['mobile'];
-
-        echo '<br><br><b>Contact No : </b><input type="number" name="contact" value='.$mobile.'>';
-       ?>
-       <br><br><b>Appointed Date : </b><input type="date" name="appointedDate">
-       <br><br><b>Time : </b><input type="time" name="time"><br><br>
-
-       <b>Short History of case : </b><textarea name="shortHistory" rows="4" cols="50"></textarea><br><br><br>
-
-      <?php
-        $consMOName = "Dr. KPN Pathirana"; // should get from a session
-        $consMOID = "687543545v";  // should get from a session
-
-        echo "<b>Name of Consultant/MO : </b>".$consMOName."<br><b> NIC of Consultant/MO : </b>".$consMOID;
 
         $_SESSION['nic'] = $nic;
         $_SESSION['force_id'] = $patientInfo['force_id'];
@@ -97,8 +117,14 @@
 
       ?>
 
-      <br><br><br> <button type="submit" name="confirm">Confirm</button>
+      <br><br><br>
+
+      <div class="button-secton">
+        <input type="submit" name="confirm">
+      </div>
 
      </form>
+   </div>
+ </div>
   </body>
 </html>

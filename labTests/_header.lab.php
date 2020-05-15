2d10fb354c290ade8122727e4bb6c3196032fa18
avishka shamendra <avishka199849@gmail.com>
2020-05-16 00:04:16 +0530

    Added Home Button To Header

--- labTests/_header.lab.php
+++ labTests/_header.lab.php
@@ -12,18 +12,23 @@ if (!$user->isLoggedIn()) {
     }
 ?>
     <header>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light shadow border-bottom border-primary ">
-            <a class="navbar-brand" href="../../views/home.php">
-                <img src="../../stylesheets/Army.png" width="45" height="35" class="d-inline-block align-top" alt="">
-            </a>
-            <p class="pt-1" style="letter-spacing:2;font-weight:bold;font-size:20px">SRI LANKA ARMY HOSPITAL</p>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav ml-auto">
+        <nav class="navbar navbar-expand navbar-light bg-light shadow border-bottom border-primary ">
+            <div class="container-fluid">
+
+                <div class="navbar-header">
+                    <a class="navbar-brand " href="../../views/home.php">
+                        <img src="../../stylesheets/Army.png" width="45" height="35" class="d-inline-block align-top" alt="Logo">
+                    </a>
+                    <a class="navbar-brand" href="../../views/home.php">Sri Lanka Army Hospital</a>
+                </div>
+
+                <ul class="navbar-nav navbar-right">
+                    <li><a href="../../views/home.php" class="btn" role="button"><b>HOME</b></a></li>
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                             <?php if ($data->user_imgstatus) {
                                 $uid = $data->user_uid;
-                                $imgSource = '../../staffprofileimgs/profileimg' . $uid . '.jpg'.'?rand=<?php echo rand();' ?>
+                                $imgSource = '../../staffprofileimgs/profileimg' . $uid . '.jpg' . '?rand=<?php echo rand();' ?>
                                 <!--only allow jpg-->
                                 <!--profile pic-->
                                 <img src=<?php echo $imgSource ?> width="25" height="25" class="d-inline-block align-top rounded-circle" alt="profile pic">
@@ -42,5 +47,7 @@ if (!$user->isLoggedIn()) {
                     </li>
                 </ul>
             </div>
+            
+        </nav>
     </header>
 <?php } ?>
--- views/_header.php
+++ views/_header.php
@@ -10,22 +10,27 @@ if (!$user->isLoggedIn()) {
         Redirect::to('../includes/Errors/404.php');
     } else {
         $data = $user->data();
-        $_SESSION['user_uid']=$data->user_uid;
+        $_SESSION['user_uid'] = $data->user_uid;
     }
 ?>
     <header>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light shadow border-bottom border-primary ">
-            <a class="navbar-brand " href="home.php">
-                <img src="../stylesheets/Army.png" width="45" height="35" class="d-inline-block align-top" alt="">
-            </a>
-            <p class="pt-1" style="letter-spacing:2;font-weight:bold;font-size:20px">SRI LANKA ARMY HOSPITAL</p>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav ml-auto">
+        <nav class="navbar navbar-expand navbar-light bg-light shadow border-bottom border-primary ">
+            <div class="container-fluid">
+
+                <div class="navbar-header">
+                    <a class="navbar-brand " href="home.php">
+                        <img src="../stylesheets/Army.png" width="45" height="35" class="d-inline-block align-top" alt="Logo">
+                    </a>
+                    <a class="navbar-brand" href="home.php">Sri Lanka Army Hospital</a>
+                </div>
+
+                <ul class="navbar-nav navbar-right">
+                    <li><a href="home.php" class="btn" role="button"><b>HOME</b></a></li>
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                             <?php if ($data->user_imgstatus) {
                                 $uid = $data->user_uid;
-                                $imgSource = '../staffprofileimgs/profileimg' . $uid . '.jpg'.'?rand=<?php echo rand();' ?>
+                                $imgSource = '../staffprofileimgs/profileimg' . $uid . '.jpg' . '?rand=<?php echo rand();' ?>
                                 <!--only allow jpg-->
                                 <!--profile pic-->
                                 <img src=<?php echo $imgSource ?> width="25" height="25" class="d-inline-block align-top rounded-circle" alt="profile pic">
@@ -43,6 +48,8 @@ if (!$user->isLoggedIn()) {
                         </div>
                     </li>
                 </ul>
+
             </div>
+        </nav>
     </header>
 <?php } ?>
